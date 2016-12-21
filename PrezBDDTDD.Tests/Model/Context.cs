fa8c5f293e360709ce6acdc423379184b62d7885
Emmanuel Lehmann <emmanuel.lehmann@axa.fr>
2016-12-21 16:47:49 +0100

    ajout de context dans les classes de steps

---
+++ PrezBDDTDD.Tests/Model/Context.cs
@@ -0,0 +1,21 @@
+﻿using PrezBDDTDD.Tests.Process;
+
+namespace PrezBDDTDD.Tests.Model
+{
+    public class Context
+    {
+        public Context()
+        {
+            srvVirement = new ServiceVirement();
+            StatutVirement = RetourVirement.Ko;
+            CompteCheque = new Compte();
+            CompteEpargne = new Compte();
+        }        
+
+        public ServiceVirement srvVirement { get; set; }
+
+        public RetourVirement StatutVirement { get; set; }
+        public Compte CompteCheque { get; set; }
+        public Compte CompteEpargne { get; set; }
+    }
+}
---
+++ PrezBDDTDD.Tests/Steps/GestionDesVirementsWhenSteps.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrezBDDTDD.Tests.Model;
+using PrezBDDTDD.Tests.Process;
+using System;
+using TechTalk.SpecFlow;
+
+namespace PrezBDDTDD.Tests.Steps
+{
+    [Binding]
+    public class GestionDesVirementsWhenSteps
+    {
+        public GestionDesVirementsWhenSteps(Context ctx)
+        {
+            this.Context = ctx;
+        }
+
+        public Context Context { get; private set; }
+        
+        
+        [When(@"j'effectue un virement de (.*)€ du compte cheque vers le compte épargne")]
+        public void WhenJEffectueUnVirementDeDuCompteChequeVersLeCompteEpargne(int montant)
+        {
+            Context.StatutVirement = Context.srvVirement.EffectuerVirement(montant, Context.CompteCheque, Context.CompteEpargne);
+        }        
+    }
+}
