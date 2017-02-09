ea2786f1b641832502f5424ac7480a0c48ddc9d7
Emmanuel Lehmann <emmanuel.lehmann@axa.fr>
2017-02-09 09:58:27 +0100

    feat(F#): création du projet F#

---
+++ dojoBDD-TDD.Tests.FS/Program.fs
@@ -0,0 +1,7 @@
+﻿// En savoir plus sur F# sur le site http://fsharp.org
+// Voir le projet 'Didacticiel F#' pour obtenir de l'aide.
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // retourne du code de sortie entier
